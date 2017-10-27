03c16e075df47beb8d9a8a9cdd1ade170cefc97f
Martinnello <32375133+Martinnello@users.noreply.github.com>
2017-10-27 17:42:44 +0200

    Ejercicio 3 y 4 ok

--- client.py
+++ client.py
@@ -7,19 +7,26 @@ import sys
 import socket
 
 
-# Constantes. Dirección IP del servidor y contenido a enviar
-SERVER = sys.argv[1]
-PORT = 6001
-LINE = '¡Hola mundo!'
+try:
+    SERVER = sys.argv[1]
+    PORT = int(sys.argv[2])
+    MESSAGE = sys.argv[3] 
+   
+    if len(sys.argv) > 3:
+        MESSAGE = ''
+        CADENA = sys.argv [3:]
+        for item in CADENA:
+            MESSAGE += ' ' + ite  
+
+except IndexError:
+    sys.exit("    Usage:   python3 client.py <Server> <Port> <Message>")
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    print("Enviando:", MESSAGE)
+    my_socket.send(bytes(MESSAGE, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
 print("Socket terminado.")
-
-# Prueba
--- server.py
+++ server.py
@@ -7,18 +7,27 @@ import sys
 import socketserver
 
 
+try:
+    PORT = int(sys.argv[1]) 
+except IndexError:
+    sys.exit("    Usage:   python3 server.py <Port>")
+
+
 class EchoHandler(socketserver.DatagramRequestHandler):
     def handle(self):      # (all requests handled by this method)
         
-        self.wfile.write("Hemos recibido tu peticion")
+        IP = self.client_address[0]
+        PORT =  self.client_address[1]
+        print("CLIENT_IP: ", IP + "\t","CLIENT_PORT: ", PORT)
+        self.wfile.write(b"Peticion recibida")
         for line in self.rfile:
-            print("El cliente nos manda ", line.decode('utf-8'))
+            print("El cliente envia: ", line.decode('utf-8'))
 
 if __name__ == "__main__":
-    serv = socketserver.UDPServer(('', 6001), EchoHandler) 
+    serv = socketserver.UDPServer(('', PORT), EchoHandler) 
 
-    print("Lanzando servidor UDP de eco...")
+    print("\n" + "Lanzando servidor UDP LOCAL - Puerto: " + str(PORT) + "\n")
     try:
         serv.serve_forever()
     except KeyboardInterrupt:
-        print("Finalizado servidor")
+        print("\n" + "Servidor finalizado")
