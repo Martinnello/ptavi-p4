f957c0bc52963e76f65a9debc595a3eeb7196097
Martinnello <32375133+Martinnello@users.noreply.github.com>
2017-10-28 13:44:07 +0200

    registered 2json añadido

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ Servidor de eco en UDP simple
 """
 import sys
 import socketserver
+import json
+import time
 
 
 try:
@@ -12,33 +14,35 @@ try:
 except IndexError:
     sys.exit("    Usage:   python3 server.py <Port>")
 
+Users = {}
+
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
+
+    def register2_json(self):       # MAKE JSON FILE
+    
+        with open("registered.json", "w") as jsonfile:
+            json.dump(Users, jsonfile, indent=3)
     
     def handle(self):      # All requests handled by this method
-
         IP = self.client_address[0]
         PORT =  self.client_address[1]
-        Users = {'USER':'', 'IP':'', 'EXPIRES':''}
         print("CLIENT_IP: ", IP + "\t","CLIENT_PORT: ", PORT)
 
-                            # Lee el registro del cliente
         Lines = self.rfile.read()
-        Lines = Lines.decode('utf-8')
-        Info = Lines.split()
+        Info = Lines.decode('utf-8').split()
         METHOD = Info[0]
         USER = Info[1].split(':')[1]
         EXPIRES = Info[-1]
+        time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))
 
         if METHOD == 'REGISTER':
-            Users['USER'] = USER
-            Users['IP'] = IP
-            Users['EXPIRES'] = EXPIRES
+            Users[USER] = ["IP: " + str(IP), "EXPIRES: " + str(EXPIRES)]
+            self.register2_json()
         if int(EXPIRES) == 0:
-            del Users['USER']
-            print(Users)
+            del Users[USER]
+            self.register2_json()
             self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-        else:
-            print(Users)
+        print(Users)
 
 if __name__ == "__main__":
     serv = socketserver.UDPServer(('', PORT), SIPRegisterHandler) 
