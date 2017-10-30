555c1a04cd738cbebe05ae8e71b2906e3853fec1
Martinnello <32375133+Martinnello@users.noreply.github.com>
2017-10-30 13:57:48 +0100

    PEP8 ok

--- server.py
+++ server.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import sys, socketserver, json, time
-
+import sys
+import socketserver
+import json
+import time
 
 
 try:
-    PORT = int(sys.argv[1]) 
+    PORT = int(sys.argv[1])
 except IndexError:
     sys.exit("    Usage:   python3 server.py <Port>")
+
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
 
     Users = {}
 
-    def json2registered(self):       # SEE IF IT EXIST    
+    def json2registered(self):       # SEE IF IT EXIST
         try:
             with open('registered.json', 'r') as jsonfile:
                 self.Users = json.load(jsonfile)
@@ -25,22 +28,22 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
 
         with open("registered.json", "w") as jsonfile:
             json.dump(self.Users, jsonfile, indent=3)
-    
+
     def handle(self):      # All requests handled by this method
 
         self.json2registered()
         IP = self.client_address[0]
-        PORT =  self.client_address[1]
-        print("CLIENT_IP: ", IP + "\t","CLIENT_PORT: ", PORT)
+        PORT = self.client_address[1]
+        print("CLIENT_IP: ", IP + "\t", "CLIENT_PORT: ", PORT)
 
         Lines = self.rfile.read()
         Info = Lines.decode('utf-8').split()
-        USER = Info[1].split(':')[1]
         METHOD = Info[0]
+        USER = Info[1].split(':')[1]
         EXPIRES = int(Info[-1])
         Time = '%Y-%m-%d %H:%M:%S'
 
-        if METHOD == 'REGISTER': 
+        if METHOD == 'REGISTER':
             try:
                 if EXPIRES == 0:
                     del self.Users[USER]
@@ -62,12 +65,12 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
 
             for user in Del_List:
                 del self.Users[user]
-                
-            print(self.Users)    
+
+            print(self.Users)
             self.register2_json()
 
 if __name__ == "__main__":
-    serv = socketserver.UDPServer(('', PORT), SIPRegisterHandler) 
+    serv = socketserver.UDPServer(('', PORT), SIPRegisterHandler)
     print("\n" + "Lanzando servidor UDP LOCAL - Puerto: " + str(PORT) + "\n")
 
     try:
